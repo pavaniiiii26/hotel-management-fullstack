4c7506561630e10662192174cf8ae32f12fab918
Pavani Patel <pavanigpatel@gmail.com>
2026-08-15 15:51:25 +0530

    Increase JSON body size limit to 10mb

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(cors({
 app.use(helmet());
 
 // Parse JSON bodies
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // Request logger middleware
 const logRequest = (req, res, next) => {
